baaa27fa64502b3b60f681ad8b901ce0e6d9cdac
Nihal Konda <nihal.konda@sjsu.edu>
2020-08-03 06:49:43 -0700

    heroku commit

--- src/components/elements/Image.js
+++ src/components/elements/Image.js
@@ -4,7 +4,7 @@ class Image extends React.Component {
     render(){
         return (
             <img alt="" className={this.props.className} src={this.props.src}  
-                onError={(e)=>{e.target.onerror = null; e.target.src="https://www.umass.edu/art/sites/default/files/store/img/cfm/plain-white-background_1.jpg"}}
+                onError={(e)=>{e.target.onerror = null; e.target.src="https://www.transparenttextures.com/patterns/asfalt-light.png"}}
             />
         )
     }
